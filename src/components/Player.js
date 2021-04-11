97a19e25b7f7463bec2f601beff7dde1359f7aae
Maris Pulk <marispulk@gmail.com>
2021-04-11 15:13:43 +0300

    Add State and Ref
    * Add State to App
    * Add Ref to App
    * Import useRef to Player
    * Insert dynamic fields and currentSong state to Song
    * Add Event handlers
    * Song design refinement
    * Headings styling

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useRef} from 'react';
 // Import component
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // Import specific item
 import { faPlay, faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 
-const Player = () => {
+const Player = ({ currentSong }) => { // Add currentSong access to Player
+    // Ref - To select specific HTML tag in component, use Ref
+    const audioRef = useRef(null); // Null is a starting value
+    // Event handlers
+    const playSongHandler = () => {
+        console.log(audioRef.current); // Get current element (song audio)
+    }
     return(
         <div className="player">
             <div className="time-control">
@@ -14,9 +20,11 @@ const Player = () => {
             </div>
             <div className="play-control">
                 <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} /> {/* Display faAngleLeft icon*/}
-                <FontAwesomeIcon className="play" size="2x" icon={faPlay} /> {/* Display faPlay icon*/}
+                <FontAwesomeIcon onClick={playSongHandler} className="play" size="2x" icon={faPlay} /> {/* Display faPlay icon*/} {/* Add playSongHandler */}
                 <FontAwesomeIcon className="skip-forward" size="2x" icon={faAngleRight} /> {/* Display faAngleRight icon*/}
             </div>
+            <audio ref={audioRef} // Pass audioRef property
+            src={currentSong.audio}></audio> {/* Pass  along currentSong */}
         </div>
     )
 }
